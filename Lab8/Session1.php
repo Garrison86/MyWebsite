7434643cd346f7f76ad422509a57905a4f830117
Michael Garrison <104579966+Garrison86@users.noreply.github.com>
2022-07-26 14:08:56 -0400

    Updated Sessions for LAB 8

--- Lab8/Session1.php
+++ Lab8/Session1.php
@@ -1,32 +1,34 @@
 <?php
-
+// user input to store variables
 include "Header.php";
 
-if (isset($_POST["submit"])) //=======================Emp Name
+if (isset($_POST["submit"]))
+{ 
     $_SESSION["empName"] = $_POST["empNameTextBox"];
-$_SESSION["empId"] = $_POST["IDTextBox"];
-$_SESSION["empTel"] = $_POST["TeleTextBox"];
-$_SESSION["empEmail"] = $_POST["EmailTextBox"];
-$_SESSION["newPos"] = $_POST["position"];
+    $_SESSION["empId"] = $_POST["IDTextBox"];
+    $_SESSION["empTel"] = $_POST["TeleTextBox"];
+    $_SESSION["empEmail"] = $_POST["EmailTextBox"];
+    $_SESSION["newPos"] = $_POST["position"];
 
 header("Location: Session2.php");
 exit;
+}
 ?>
 
 <div class="row container-fluid">
     <div class="left-side col">
         <br>
+        <form method="post" action="Session2.php">
         <div class="col">
-            <form method="post" action="Session2.php">
-                <input type="text" name="empName" placeholder="Employee Name?" style="margin:2px;" />
+                <input type="text" name="empNameTextBox" placeholder="Employee Name?" style="margin:2px;" />
 
-                <input type="text" name="empId" placeholder="Employee ID?" style="margin:2px;" />
+                <input type="text" name="IDTextBox" placeholder="Employee ID?" style="margin:2px;" />
 
-                <input type="text" name="empTel" placeholder="Telephone Number?" style="margin:2px;" />
+                <input type="text" name="TeleTextBox" placeholder="Telephone Number?" style="margin:2px;" />
 
-                <input type="text" name="empEmail" placeholder="Email Address?" style="margin:2px;" />
-        </div>
-
+                <input type="text" name="EmailTextBox" placeholder="Email Address?" style="margin:2px;" />
+            </div>
+            
         <div class="col">
             <input type="radio" name="position" value="Manager" />
             <label for="empNameTextBox">Manager</label>
@@ -36,10 +38,10 @@ exit;
             <label for="empNameTextBox">IT Developer</label>
             <input type="radio" name="position" value="iTAnalyst" />
             <label for="empNameTextBox">IT Analyst</label>
+            <br>
+            <input type="submit" value="Submit Information" />
         </div>
-        <br>
-        <input type="submit" value="Submit Information" />
-        </form>
+    </form>
     </div>
 </div>
 
