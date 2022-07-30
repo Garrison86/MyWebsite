d929f06c4a9df13d6b776a850aa1c1fb224df656
Michael Garrison <104579966+Garrison86@users.noreply.github.com>
2022-07-30 13:23:12 -0400

    changes to Lab 9 PHP

--- Lab_9_PHP_MySQL/MySQLConnectionInfo.php
+++ Lab_9_PHP_MySQL/MySQLConnectionInfo.php
@@ -1,14 +1,20 @@
-<!-- <?php
-// user input to store variables
-$host = 'localhost';
-$username = 'ugntm1olmpap0';
-$password = 'gzh1aroumisr';
-$database = 'dbuoykhmo6i7yu';
-?> -->
 <?php
 // user input to store variables
-$host = 'localhost';
-$username = 'root';
+// $host = 'localhost';
+// $username = 'ugntm1olmpap0';
+// $password = 'gzh1aroumisr';
+// $database = 'dbuoykhmo6i7yu';
+
+// user input to store variables
+// $host = 'localhost';
+// $username = 'root';
+// $password = '';
+// $database = 'dbuoykhmo6i7yu';
+
+
+// user input to store variables
+$host = '127.0.0.1';
+$username = 'ugntm1olmpap0';
 $password = '';
-$database = 'test';
+$database = 'dbuoykhmo6i7yu';
 ?>
--- Lab_9_PHP_MySQL/ViewAllEmployees.php
+++ Lab_9_PHP_MySQL/ViewAllEmployees.php
@@ -7,15 +7,16 @@ try {
     // $pdo = new PDO("mysql:host=$host;dbname=$database", $username, $password);
     // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-    $mysqli = new mysqli('127.0.0.1', 'root', '', 'test');
+    $mysqli = new mysqli($host, $username, $password, $database);
+    // $mysqli = new mysqli($host, $username, $password, $database);
     echo "Connected successfully" . "</br>";
 
-    $sqlQuery = "SELECT * FROM employees";	// CONNECTION
+    $sqlQuery = "SELECT * FROM employee";	// CONNECTION
     $result = $mysqli->query($sqlQuery); 
     $outputInArrayFormat = $result->fetch_all(MYSQLI_ASSOC);
-    print_r($outputInArrayFormat);
+    //print_r($outputInArrayFormat);
     foreach ($outputInArrayFormat as $employee ){
-		echo $employee['firstName'];		
+		echo $employee['FirstName'];		
     }
     }
     catch (PDOException $e) {
@@ -30,13 +31,44 @@ include "Header.php";
     <h3>Session State Data</h3>
 
     </div>
-    <?php foreach ($outputInArrayFormat as $employee ):?>
-    <div class="row" style="background-color:white; height: 250px; margin: 5px;">
-    <h3>Database Data</h3>
-    <h2><?php echo $employee['empFirstName'];?></h2>
+    <div class="row" style="background-color:white; height: 250px; height:fit-content; margin: 5px;">
+        <h3>Database Data</h3>
+        <div class="col">
+        <h4>First Name</h4>
+            <?php foreach ($outputInArrayFormat as $employee ):?>
+            <h4><?php echo $employee['FirstName'].'<br>';?></h4>
+            <?php endforeach; ?>
+        </div>
+        <div class="col">
+        <h4>Last Name</h4>
+            <?php foreach ($outputInArrayFormat as $employee ):?>
+            <h4><?php echo $employee['LastName'].'<br>';?></h4>
+            <?php endforeach; ?>
+        </div>
+        <div class="col">
+            <h4>Email Address</h4>
+            <?php foreach ($outputInArrayFormat as $employee ):?>
+            <h4><?php echo $employee['EmailAddress'].'<br>';?></h4>
+            <?php endforeach; ?>
+        </div>
+        <div class="col">
+        <h4>Phone Number</h4>
+            <?php foreach ($outputInArrayFormat as $employee ):?>
+            <h4><?php echo $employee['FirstName'].' '.$employee['LastName'].''.'<br>';?></h4>
+            <?php endforeach; ?>
+        </div>
+        <div class="col">
+        <h4>SIN</h4>
+            <?php foreach ($outputInArrayFormat as $employee ):?>
+            <h4><?php echo $employee['FirstName'].' '.$employee['LastName'].''.'<br>';?></h4>
+            <?php endforeach; ?>
+        </div>
+        <div class="col">
+        <h4>Password</h4>
+            <?php foreach ($outputInArrayFormat as $employee ):?>
+            <h4><?php echo $employee['PASSWORD'].'<br>';?></h4>
+            <?php endforeach; ?>
+        </div>
     </div>
-        <?php endforeach; ?>
 </div>
-<?php
-include "Footer.php";
-?>
+<?php include "Footer.php"; ?>
