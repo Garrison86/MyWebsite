ad9f4ec36428c1c80a9510ba61789280f8133e99
Michael Garrison <104579966+Garrison86@users.noreply.github.com>
2022-08-08 21:54:12 -0400

    Assigment 3

---
+++ Assignemt_3/db_config.php
@@ -0,0 +1,14 @@
+<?php 
+// $servername = "localhost";
+// $username = "eatery";
+// $password = "cst@8238";
+// $dbname = "eatery";
+
+//SiteGround Info-you must modify the Database 
+// and Username as generated automatically by the system of the web hosting domain.
+$servername = "localhost";
+$username = "ujrfgcl0tsqbq";
+$password = "1234Eatery";
+$dbname = "dbsbzgigqz1kqd";
+
+?>
