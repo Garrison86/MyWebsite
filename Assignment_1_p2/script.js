a33eae4b6f64faf96452e257f6f42f699eec7796
Michael Garrison <104579966+Garrison86@users.noreply.github.com>
2023-07-29 17:50:11 -0400

    Update script.js

--- Assignment_1_p2/script.js
+++ Assignment_1_p2/script.js
@@ -1,12 +1,11 @@
 
 var myJsOutput = document.getElementById("myJsOutput");
-//var fibOutput = document.getElementById("fibOutput"); // not used 
-//var hollOutput = document.getElementById("hollOutput"); // not used 
+var fibOutput = document.getElementById("fibOutput"); 
+var hollOutput = document.getElementById("hollOutput"); 
 
-/** Button Function - (problem with having all options available at once)**/
 document.getElementById("myButton").onclick = function(){
     myJsOutput.innerHTML = "<br>";
-    tri = document.getElementById("myText").value;
+    var tri = document.getElementById("myText").value;
     for (var i = 0; i <= tri; i++) {
     myJsOutput.innerHTML += " \n" + "<br>";
     for (var j = 0; j < i; j++) {
@@ -15,27 +14,21 @@ document.getElementById("myButton").onclick = function(){
   }
 }
 
-/** Fibonacci Function **/
 document.getElementById("myFibButton").onclick = function(){
     myJsOutput.innerHTML = "<br>";
-    //fibOutput.innerHTML = "<br>";
-    fibSeq = document.getElementById("myFibonacci").value;
-    var n1=0,n2=1,n3,i;  // Variables for the for loop
+    var fibSeq = document.getElementById("myFibonacci").value;
+    var n1=0,n2=1,n3,i;  
     for (i=0;i<fibSeq;i++){  
      n3=n1+n2;  
-     fibOutput.innerHTML += " "+ n1 +" + "+ n2 +" = "+n3 +"<br>";  
+     fibOutput.innerHTML += " "+ n1 +" + "+n2+" = "+n3 +"<br>";  
      n1=n2;  
      n2=n3;  
     } 
 }
 
-
-
-
-/** hollow rectangle courtesy of https://www.tutorialstonight.com/online-html-editor.php?p=js&q=hollow-square-star-pattern*/
 document.getElementById("myHollowBtn").onclick = function(){
-   let hollow = document.getElementById("myHollow").value;
-    let string = "";
+   var hollow = document.getElementById("myHollow").value;
+   let string = "";
     
     for (let i = 0; i < hollow; i++) {
         for (let j = 0; j < hollow; j++) {
@@ -53,6 +46,5 @@ document.getElementById("myHollowBtn").onclick = function(){
         }
       string += "<br>";
     }
-    myJsOutput.innerHTML = ("<pre>"+ string +"</pre>");
-    //hollOutput.innerHTML = ("<pre>"+ string +"</pre>");
+    hollOutput.innerHTML = ("<pre>"+ string +"</pre>");
 }
